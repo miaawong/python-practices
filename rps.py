29356e71a2380a2e47fb45bc3e3f836b29667011
Mia Wong <miaw629@gmail.com>
2019-09-12 22:33:21 -0400

    rps in python for coderschool

---
+++ rps.py
@@ -0,0 +1,32 @@
+import random
+options = ["Rock", "Paper", "Scissors"]
+
+computer = options[random.randint(0, 2)]
+
+player = input("Rock, Paper, or Scissors")
+if player == computer:
+    print('Tie!')
+elif player == "Rock":
+    if computer == "Paper":
+        print('You win!')
+    else:
+        print('lost')
+elif player == "Paper":
+    if computer == "Scissors":
+        print('You lose')
+    else:
+        print('You win')
+elif player == "Scissors":
+    if computer == "Rock":
+        print('You lost')
+    else:
+        print('You win')
+else:
+    print("That not a valid!")
+
+
+# prompt user, rock paper scissors
+
+# take the input of the user and store it into a variable
+
+# build bot's choice: needs to choose random rock paper scissors if()
